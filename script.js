19cca97bf913775b0671f0a66e19e07269d3e4ec
AbdenourBout <32998482+AbdenourBout@users.noreply.github.com>
2018-08-31 23:24:43 +0100

    first

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() {
+
+  $.scrollify({
+
+    section: "section",
+
+  });
+
+});
+function showResult(){
+
+  for (var i = 0; i < 20; i++) {
+        var line = $("<h1>dummy text</h1>");
+        $("#res").append(line);
+    }
+      
+}
